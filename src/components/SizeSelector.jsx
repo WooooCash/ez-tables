24ae42457bdb6a0bd2e42b3d9d1a64e114c47283
lbialczak <lbialczak@grupazpr.pl>
2023-05-09 06:53:39 +0200

    size selector styling

--- src/components/SizeSelector.jsx
+++ src/components/SizeSelector.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import css from "../css/SizeSelector.module.css";
 
 const maxRows = 10;
 const maxCols = 10;
@@ -15,24 +16,29 @@ const SizeSelector = () => {
   return (
     <>
       {!picked && (
-        <div>
-          {[...Array(maxRows)].map((e, row) => (
-            <div>
-              {[...Array(maxCols)].map((e2, col) => (
-                <div 
-                  className="selector"
-                  onMouseEnter={() => setSize({ rows: row + 1, cols: col + 1 })}
-                  onClick={() => setPicked(true)}
-                >
-                  
-                </div>
-              ))}
-            </div>
-          ))}
+        <>
+          <div onMouseLeave={() => setSize({ rows: 0, cols: 0 })}>
+            {[...Array(maxRows)].map((e, row) => (
+              <div className={css.selectorRow}>
+                {[...Array(maxCols)].map((e2, col) => (
+                  <div
+                    className={[
+                      css.selector,
+                      row < size.rows && col < size.cols ? css.selected : "",
+                    ].join(" ")}
+                    onMouseEnter={() =>
+                      setSize({ rows: row + 1, cols: col + 1 })
+                    }
+                    onClick={() => setPicked(true)}
+                  ></div>
+                ))}
+              </div>
+            ))}
+          </div>
           <div>
             ({size.cols}, {size.rows})
           </div>
-        </div>
+        </>
       )}
     </>
   );
